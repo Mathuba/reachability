d4ceb713cfbff23bc94d0d2320a3f7bdea95500a
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-01 14:28:14 +0200

    Added functionality to build a graph

--- reachability.py
+++ reachability.py
@@ -2,29 +2,32 @@
 
 import sys
 
-def create_graph(graph=None):
-    n, m = map(int, sys.stdin.readline().split())
-    graph = {vertex: [] for vertex in range(1, n+1)}
-
-    for _ in range(m):
-        vert1, vert2 = map(int, sys.stdin.readline().split())
-        add_adge(graph(vertex1, vertex2))
-    return graph
+def add_edge(graph, vertex1, vertex2):
+    edges = (vertex1, vertex2)
+    (v1, v2) = (edges)
+    if v1 in graph:
+        if v2 not in graph[v1]:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+    else:
+        graph[v1] = [v2]
 
 def reach(adj, x, y):
     #write your code here
     return 0
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n, m = data[0:2]
-    data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
-    x, y = data[2 * m:]
-    adj = [[] for _ in range(n)]
-    x, y = x - 1, y - 1
-    for (a, b) in edges:
-        adj[a - 1].append(b - 1)
-        adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+
+    n, m = map(int, sys.stdin.readline().split())
+    graph = {vertex: [] for vertex in range(1, n+1)}
+
+    for _ in range(m):
+        vert1, vert2 = map(int, sys.stdin.readline().split())
+        add_edge(graph, vert1, vert2)
+    
+    start_vert, end_vert = map(int, sys.stdin.readline().split())
+    print("start_vert: ", start_vert)
+    print("end_vert: ", end_vert)
+    print(graph)
+
+    # print(reach(adj, x, y))
