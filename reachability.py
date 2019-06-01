60ecdc79d89f40bbd01ecd243b8d23e45094bddd
Mathuba Dlamini <Mathuba58@gmail.com>
2019-06-01 14:42:26 +0200

    Added functionality to check if path exists between two vertices

--- reachability.py
+++ reachability.py
@@ -12,8 +12,18 @@ def add_edge(graph, vertex1, vertex2):
     else:
         graph[v1] = [v2]
 
-def reach(adj, x, y):
-    #write your code here
+def reach(graph, st_vert, end_vert, visited=None):
+    if visited is None:
+        visited = []
+    visited = visited + [st_vert]
+    if st_vert == end_vert:
+            return 1
+    
+    for neighbor_vert in graph[st_vert]:
+            if neighbor_vert not in visited:
+                new_visitor = reach(graph, neighbor_vert, end_vert, visited)
+                if new_visitor:
+                    return 1
     return 0
 
 if __name__ == '__main__':
@@ -26,8 +36,4 @@ if __name__ == '__main__':
         add_edge(graph, vert1, vert2)
     
     start_vert, end_vert = map(int, sys.stdin.readline().split())
-    print("start_vert: ", start_vert)
-    print("end_vert: ", end_vert)
-    print(graph)
-
-    # print(reach(adj, x, y))
+    print(reach(graph, start_vert, end_vert))
